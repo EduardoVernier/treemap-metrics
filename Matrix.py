99f7161488651a4109b46143b985577f0794c95a
Eduardo Vernier <efvernier@gmail.com>
2018-03-22 13:23:31 -0300

    Matrices for the 3 metrics.

--- Matrix.py
+++ Matrix.py
@@ -21,12 +21,27 @@ acronyms = {
 
 
 def plot(dataset_ids):
-
+    # Plot AR matrix
     ar_matrix, technique_acronyms = make_ar_matrix(dataset_ids)
+    plot_matrix(ar_matrix, dataset_ids, technique_acronyms, 'ar')
+
+    # Plot CT matrix
+    ct_matrix, technique_acronyms = make_ct_matrix(dataset_ids)
+    plot_matrix(ct_matrix, dataset_ids, technique_acronyms, 'ct')
+
+    # Plot RPC matrix
+    rpc_matrix, technique_acronyms = make_rpc_matrix(dataset_ids)
+    plot_matrix(rpc_matrix, dataset_ids, technique_acronyms, 'rpc')
+
+
+def plot_matrix(matrix, dataset_ids, technique_acronyms, metric_id):
 
     fig = plt.figure()
     ax = fig.add_subplot(111)
-    mat = ax.matshow(ar_matrix, cmap=plt.cm.viridis)
+    if metric_id == 'ar':
+        mat = ax.matshow(matrix, cmap=plt.cm.viridis)
+    else:
+        mat = ax.matshow(matrix, cmap=plt.cm.viridis_r)  # Invert colormap
 
     # Ticks, labels and grids
     ax.set_xticklabels(dataset_ids, rotation='vertical')
@@ -40,9 +55,71 @@ def plot(dataset_ids):
 
     fig.colorbar(mat)
     fig.tight_layout()
-    fig.savefig("matrix-ar.png", dpi=600)
+    fig.savefig('matrix-'+ metric_id +'.png', dpi=600)
     # plt.show()
-    a = 1
+
+
+
+def make_ct_matrix(dataset_ids):
+
+    technique_ids = []
+    all_means = []
+    for dataset_id in dataset_ids:
+        ct_df = Parser.read_ct_metric(dataset_id)
+
+        dataset_means = np.array([])
+        technique_list = sorted(ct_df)
+        if len(technique_ids) == 0:
+            technique_acronyms = [acronyms[d] for d in technique_list]
+
+        for i, technique_id in enumerate(technique_list):
+            technique_means = []
+            for revision in range(int(len(ct_df[technique_id].columns) / 2)):
+                df = ct_df[technique_id]
+                r_col = df.columns[2 * revision]
+                b_col = df.columns[2 * revision + 1]
+
+                diff = df[[r_col, b_col]].max(axis=1) - df[b_col]
+                diff = diff.dropna()
+                diff_mean = diff.mean()
+
+                technique_means.append(diff_mean)
+
+            dataset_means = np.append(dataset_means, np.mean(technique_means))
+        all_means.append(dataset_means)
+
+    return np.array(all_means).transpose(), technique_acronyms  # Transpose matrix so each row is a technique and each column a dataset
+
+
+def make_rpc_matrix(dataset_ids):
+
+    technique_ids = []
+    all_means = []
+    for dataset_id in dataset_ids:
+        rpc_df = Parser.read_rpc_metric(dataset_id)
+
+        dataset_means = np.array([])
+        technique_list = sorted(rpc_df)
+        if len(technique_ids) == 0:
+            technique_acronyms = [acronyms[d] for d in technique_list]
+
+        for i, technique_id in enumerate(technique_list):
+            technique_means = []
+            for revision in range(int(len(rpc_df[technique_id].columns) / 2)):
+                df = rpc_df[technique_id]
+                r_col = df.columns[2 * revision]
+                b_col = df.columns[2 * revision + 1]
+
+                diff = df[[r_col, b_col]].max(axis=1) - df[b_col]
+                diff = diff.dropna()
+                diff_mean = diff.mean()
+
+                technique_means.append(diff_mean)
+
+            dataset_means = np.append(dataset_means, np.mean(technique_means))
+        all_means.append(dataset_means)
+
+    return np.array(all_means).transpose(), technique_acronyms  # Transpose matrix so each row is a technique and each column a dataset
 
 
 def make_ar_matrix(dataset_ids):
@@ -56,6 +133,7 @@ def make_ar_matrix(dataset_ids):
         technique_list = sorted(ar_df)
         if len(technique_ids) == 0:
             technique_acronyms = [acronyms[d] for d in technique_list]
+
         for i, technique_id in enumerate(technique_list):
             technique_means = []
             for revision in range(int(len(ar_df[technique_id].columns) / 2)):
